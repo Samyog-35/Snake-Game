ab0e9a421cab59dd2b85da0d00af30fc58f19d42
Samyog Adhikari <samyogadhikari090@gmail.com>
2026-04-04 17:37:57 +0000

    Apr 4, 2026, 1:37 PM

--- scr/main.py
+++ scr/main.py
@@ -71,21 +71,21 @@ while True:
             snake.move()
                 # wall collision
             hx, hy = snake.segments[0]
-        if hx < 0 or hy < 0 or hx >= COLS or hy >= ROWS: 
-            game_over = True
-            death_cause = "You hit The wall!!!" 
+            if hx < 0 or hy < 0 or hx >= COLS or hy >= ROWS: 
+                game_over = True
+                death_cause = "You hit The wall!!!" 
 
                 #self collision 
-        if snake.segments[0]in snake.segments[1:]:
-            game_over = True
-            death_cause = "you bit yourself"
+            if snake.segments[0]in snake.segments[1:]:
+                game_over = True
+                death_cause = "you bit yourself"
 
 
                 # ate food
-        if snake.segments[0] == food.get_pos():
-            score += food.get_points()
-            snake.segments.append(snake.segments[-1])
-            food = spawn_food()
+            if snake.segments[0] == food.get_pos():
+                score += food.get_points()
+                snake.segments.append(snake.segments[-1])
+                food = spawn_food()
 
     # draw
     screen.fill((18, 20, 28))
