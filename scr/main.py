28374db10f5e0e1f53ca5b275f92556452fa2e3b
Samyog Adhikari <samyogadhikari090@gmail.com>
2026-04-06 01:10:27 +0000

    Apr 5, 2026, 9:10 PM

--- scr/main.py
+++ scr/main.py
@@ -40,14 +40,14 @@ move_delay = 0.15
 move_timer = 0.0
 
 def draw_start_screen():
- """ draws a screen """
-screen.fill((18,20,28))
-title = font.render("SNAKE Game",True,(80,200,120))
-screen.blit(title, (220, 150))
-prompt = font.render("Press Enter Key To Play",True, (225,225,25))
-screen.blit(title, (185, 230))
-controls = font.render("Arrow Key To Move",True,(130,138,160))
-screen.blit(controls,(190,280))
+    """ draws a screen """
+    screen.fill((18,20,28))
+    title = font.render("SNAKE Game",True,(80,200,120))
+    screen.blit(title, (220, 150))
+    prompt = font.render("Press Enter Key To Play",True, (225,225,25))
+    screen.blit(title, (185, 230))
+    controls = font.render("Arrow Key To Move",True,(130,138,160))
+    screen.blit(controls,(190,280))
 
  
 
