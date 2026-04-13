b547b2ab4fa0ee1a7193b6b3669521753a8b0015
Samyog Adhikari <samyogadhikari090@gmail.com>
2026-04-13 03:31:50 +0000

    Apr 12, 2026, 11:31 PM

--- scr/main.py
+++ scr/main.py
@@ -130,7 +130,7 @@ while True:
             death_cause = "You starved to death!" 
         if speed_boost_timer[0] > 0:
             speed_boost_timer[0] -= dt
-            move_delay = 0.09
+            move_delay = 0.18
          # slow down as timer runs out
         else:
             move_delay = 0.18 + (10.00 - starve_timer) * 0.015
